8cee8bcf8e16340ffe0b61da84b8d29446988c37
razacode <asif756raza@gmail.com>
2025-07-26 00:19:54 +0530

    react add

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signup from './Signup';
+import './Signup.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
